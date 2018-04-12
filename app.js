06c9dabe2c6f841fb4c37c80dc67033241942ec4
Tosha31 <tommywebacct@gmail.com>
2018-04-12 02:53:31 -0500

    Remove empty array elements(empty divs)

--- app.js
+++ app.js
@@ -10,9 +10,15 @@ request(url, function (err, res, html){
     var allItems = $('#siteTable').children();
     var items = [];
     allItems.each(function (index){
-      items.push($('#siteTable').children().eq(index).children().eq(4).find('a.title').text())
+
+     var result = $('#siteTable').children().eq(index).children().eq(4).find('a.title').text();
+     if(result !== "") { //if result is not empty string the push to array
+      items.push(result);
+    }
+    
 
     });
     console.log(items);
   }
 });
+
